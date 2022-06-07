e560bfbbe25a06781c3f06f06423444385062df4
Stefan Haustein <haustein@google.com>
2022-06-07 06:01:13 +0200

    Internal change
    PiperOrigin-RevId: 453345883

--- kmpbench/com/google/apps/xplat/kmpbench/app/ios/BenchmarkTest.kt
+++ kmpbench/com/google/apps/xplat/kmpbench/app/ios/BenchmarkTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.google.apps.xplat.kmpbench.app.ios
 
 import com.google.apps.xplat.kmpbench.combination.BenchmarkManager
@@ -34,8 +50,8 @@ class BenchmarkTest {
         ok = ok && relative >= expectedPerformance
       }
 
-      val message = 
-          "Benchmark $name (ops/s) Kotlin: $kotlinResult; Java: $javaResult; Relative: $relative\n"
+      val message =
+        "Benchmark $name (ops/s) Kotlin: $kotlinResult; Java: $javaResult; Relative: $relative\n"
       println(message)
       results.append(message)
     }
