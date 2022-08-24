5a615a32ea5741231772014eef33bb1e75f52add
Martin Kretzschmar <mkretzschmar@google.com>
2022-08-24 00:03:49 -0700

    Add a `@KtNative` declaration for `Comparators`. Before, it could only be used from Kotlin but the `java.util.Collections` implementation needs to use it from Java.
    PiperOrigin-RevId: 469650076

---
+++ j2kt/jre/java/native/java/util/Comparators.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package java.util;
+
+import javaemul.internal.annotations.KtNative;
+
+@KtNative("java.util.Comparators")
+class Comparators {
+  /*
+   * This is a utility class that provides default Comparators. This class
+   * exists so Arrays and Collections can share the natural comparator without
+   * having to know internals of each other.
+   *
+   * This class is package protected since it is not in the JRE.
+   */
+
+  static native <T> Comparator<T> nullToNaturalOrder(Comparator<T> cmp);
+
+  private Comparators() {}
+}
