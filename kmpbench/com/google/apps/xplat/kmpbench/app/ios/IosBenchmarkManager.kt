4f659a40e8aab3eb21e152758752fa2f550fd1a7
Mark Mann <markmann@google.com>
2023-10-16 08:02:27 -0700

    Internal cleanup.
    PiperOrigin-RevId: 573811110

--- kmpbench/com/google/apps/xplat/kmpbench/app/ios/IosBenchmarkManager.kt
+++ kmpbench/com/google/apps/xplat/kmpbench/app/ios/IosBenchmarkManager.kt
@@ -17,6 +17,7 @@
 package com.google.apps.xplat.kmpbench.app.ios
 
 import com.google.apps.xplat.kmpbench.combination.BenchmarkManager
+import third_party.java_src.j2objc.jre_emul_jre_core.JavaUtilMapProtocol
 import third_party.java_src.j2objc.jre_emul_jre_core.JavaUtilMap_EntryProtocol
 import third_party.java_src.xplat.kmpbench.java_benchmarks_j2objc.ComGoogleJ2clBenchmarkingFrameworkAbstractBenchmark
 import third_party.java_src.xplat.kmpbench.java_benchmarks_j2objc.ComGoogleJ2clBenchmarkingFrameworkBenchmarkExecutor
@@ -24,7 +25,8 @@ import third_party.java_src.xplat.kmpbench.java_benchmarks_j2objc.ComGoogleJ2clB
 
 fun createIosBenchmarkManager(): BenchmarkManager {
   val javaMap = mutableMapOf<String, () -> Double>()
-  val iterator = ComGoogleJ2clBenchmarksAllBenchmarks_get_map()!!.entrySet().iterator()
+  val iterator =
+    (ComGoogleJ2clBenchmarksAllBenchmarks_get_map() as JavaUtilMapProtocol).entrySet().iterator()
   while (iterator.hasNext()) {
     val entry = iterator.next() as JavaUtilMap_EntryProtocol
     val javaBenchmark = entry.getValue() as ComGoogleJ2clBenchmarkingFrameworkAbstractBenchmark
