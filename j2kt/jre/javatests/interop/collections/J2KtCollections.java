b960c8c2096b401a909b361ec48aaa7f4fd3fb3f
Xplat Team <no-reply@google.com>
2023-10-26 09:03:36 -0700

    Add basic KotlinNative-J2KT interop tests for collections, which captures the current state of the transpiled J2KT code.
    PiperOrigin-RevId: 576889658

---
+++ j2kt/jre/javatests/interop/collections/J2KtCollections.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package interop.collections;
+
+import static java.util.Collections.unmodifiableList;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jspecify.nullness.NullMarked;
+
+/** Exposes collection-related methods for the corresponding interop test. */
+@NullMarked
+public class J2KtCollections {
+
+  /** Accepts any list. */
+  public static <T> void accept(List<T> list) {}
+
+  /** Returns new list which is mutable. */
+  public static <T> List<T> newMutableList() {
+    return new ArrayList<>();
+  }
+
+  /** Returns new list which is read-only. */
+  public static <T> List<T> newReadonlyList() {
+    return unmodifiableList(newMutableList());
+  }
+}
---
+++ j2kt/jre/javatests/interop/collections/J2KtCollectionsInteropTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package interop.collections
+
+import kotlin.test.Test
+
+/** Tests interop between Kotlin and J2KT-generated code. */
+class J2KtCollectionsInteropTest {
+
+  // At this moment, J2KT collections map to mutable collections in Kotlin, so it's not possible to
+  // pass read-only collections as arguments to J2KT methods.
+  @Test
+  fun j2kt_acceptsKotlinList() {
+    // J2KtCollections.accept(listOf("foo", "bar"))
+  }
+
+  @Test
+  fun j2kt_acceptsKotlinMutableList() {
+    J2KtCollections.accept(mutableListOf("foo", "bar"))
+  }
+
+  @Test
+  fun j2ktMutableList_assignsToList() {
+    val unused: List<String> = J2KtCollections.newMutableList()
+  }
+
+  // At this moment, collections from J2KT can be assigned to mutable collections in Kotlin.
+  @Test
+  fun j2ktMutableList_assignsToMutableList() {
+    val unused: MutableList<String> = J2KtCollections.newMutableList()
+  }
+
+  @Test
+  fun j2ktMutableList_kotlinMutationSupported() {
+    val list = J2KtCollections.newMutableList<String>()
+    list.add("foo")
+  }
+
+  @Test
+  fun j2ktReadonlyList_assignsToKotlinList() {
+    val unused: List<String> = J2KtCollections.newReadonlyList()
+  }
+
+  // At this moment, collections from J2KT can be assigned to mutable collections in Kotlin.
+  @Test
+  fun j2ktReadonlyList_assignsToKotlinMutableList() {
+    val unused: MutableList<String> = J2KtCollections.newReadonlyList()
+  }
+
+  // At this moment, collections from J2KT are mutable, so mutation is allowed but unsupported.
+  @Test
+  fun j2ktReadonlyList_kotlinMutationUnsupported() {
+    val list: MutableList<String> = J2KtCollections.newReadonlyList()
+    try {
+      list.add("foo")
+    } catch (e: UnsupportedOperationException) {
+      // Mutation is unsupported
+    }
+  }
+}
