7aee64e56a650e2b01e5b18bc82b05dc6824134c
Stefan Haustein <haustein@google.com>
2024-08-30 03:51:43 -0700

    Add minimal JRE Base64 emulation support to j2kt
    PiperOrigin-RevId: 669270526

---
+++ j2kt/jre/java/native/java/util/Base64.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package java.util;
+
+import javaemul.internal.annotations.KtNative;
+import org.jspecify.annotations.NullMarked;
+
+/** Partial J2cl emulation of Base64. */
+@KtNative(name = "java.util.Base64") // Reimplemented in Kotlin, not transpiled.
+@NullMarked
+public final class Base64 {
+
+  private Base64() {}
+
+  public static native Decoder getDecoder();
+
+  public static native Encoder getEncoder();
+
+  @KtNative(name = "java.util.Base64.Decoder")
+  public static class Decoder {
+    Decoder() {}
+
+    public native byte[] decode(String s);
+  }
+
+  @KtNative(name = "java.util.Base64.Encoder")
+  public static class Encoder {
+    Encoder() {}
+
+    public native String encodeToString(byte[] b);
+  }
+}
---
+++ j2kt/jre/java/native/java/util/Base64.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package java.util
+
+import kotlin.experimental.ExperimentalObjCName
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.native.ObjCName
+
+@OptIn(ExperimentalObjCName::class)
+@ObjCName("J2ktBase64")
+class Base64 internal constructor() {
+
+  class Decoder internal constructor() {
+    fun decode(s: String): ByteArray {
+      // Note that we are using an experimental API here for convenience and to reduce overhead, but
+      // it would be relatively straightforward to implement base64 "manually" if required.
+      @OptIn(ExperimentalEncodingApi::class)
+      return Base64.Default.decode(s)
+    }
+  }
+
+  class Encoder internal constructor() {
+
+    fun encodeToString(b: ByteArray): String {
+      val sb = StringBuilder()
+      @OptIn(ExperimentalEncodingApi::class) Base64.Default.encodeToAppendable(b, sb)
+      return sb.toString()
+    }
+  }
+
+  companion object {
+    val decoder = Decoder()
+    val encoder = Encoder()
+
+    fun getDecoder(): Decoder = decoder
+
+    fun getEncoder(): Encoder = encoder
+  }
+}
