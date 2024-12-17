8eb4b7da708e479ee0b8e14638c00a309873392b
Martin Kretzschmar <mkretzschmar@google.com>
2024-12-17 01:48:12 -0800

    Remove J2ktMonitor workaround that was necessary for incorrect internal j2kt-jvm bazel rules.
    PiperOrigin-RevId: 707004093

--- j2kt/jre/java/jvm/javaemul/lang/J2ktMonitor.kt
+++ j2kt/jre/java/jvm/javaemul/lang/J2ktMonitor.kt
@@ -15,7 +15,4 @@
  */
 package javaemul.lang
 
-// TODO(b/260232556): Turning this into a typealias for Any causes a
-// "java.lang.ClassNotFoundException:javaemul.lang.J2ktMonitor" for
-// third_party/java_src/j2cl/transpiler/javatests/com/google/j2cl/integration/java/synchronizedblock:j2kt_jvm_test
-open class J2ktMonitor
+typealias J2ktMonitor = Any
