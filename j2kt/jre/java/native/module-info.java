5cb35dbd607fed35f95153224b54ce6bb469bf12
Xplat Team <no-reply@google.com>
2024-12-16 17:08:01 -0800

    Add typealias from JRE WeakReference to kotlin.native.ref.WeakReference.
    PiperOrigin-RevId: 706882934

--- j2kt/jre/java/native/module-info.java
+++ j2kt/jre/java/native/module-info.java
@@ -15,6 +15,7 @@ module java.base {
   exports java.io;
   exports java.lang.annotation;
   exports java.lang.invoke;
+  exports java.lang.ref;
   exports java.lang.reflect;
   exports java.lang;
   exports java.math;
