460533ad6ea2997fb5292c59de579a3af75ae438
Xplat Team <no-reply@google.com>
2025-11-11 05:52:14 -0800

    [J2KT] Remove `@HiddenFromObjC` from static final fields in JRE.
    PiperOrigin-RevId: 830875156

--- j2kt/jre/java/native/java/lang/Character.java
+++ j2kt/jre/java/native/java/lang/Character.java
@@ -39,65 +39,65 @@ public final class Character implements Comparable<Character> {
 
   public static final Class<Character> TYPE = ktNative();
 
-  @HiddenFromObjC public static final byte UNASSIGNED = 0;
+  public static final byte UNASSIGNED = 0;
 
-  @HiddenFromObjC public static final byte UPPERCASE_LETTER = 1;
+  public static final byte UPPERCASE_LETTER = 1;
 
-  @HiddenFromObjC public static final byte LOWERCASE_LETTER = 2;
+  public static final byte LOWERCASE_LETTER = 2;
 
-  @HiddenFromObjC public static final byte TITLECASE_LETTER = 3;
+  public static final byte TITLECASE_LETTER = 3;
 
-  @HiddenFromObjC public static final byte MODIFIER_LETTER = 4;
+  public static final byte MODIFIER_LETTER = 4;
 
-  @HiddenFromObjC public static final byte OTHER_LETTER = 5;
+  public static final byte OTHER_LETTER = 5;
 
-  @HiddenFromObjC public static final byte NON_SPACING_MARK = 6;
+  public static final byte NON_SPACING_MARK = 6;
 
-  @HiddenFromObjC public static final byte ENCLOSING_MARK = 7;
+  public static final byte ENCLOSING_MARK = 7;
 
-  @HiddenFromObjC public static final byte COMBINING_SPACING_MARK = 8;
+  public static final byte COMBINING_SPACING_MARK = 8;
 
-  @HiddenFromObjC public static final byte DECIMAL_DIGIT_NUMBER = 9;
+  public static final byte DECIMAL_DIGIT_NUMBER = 9;
 
-  @HiddenFromObjC public static final byte LETTER_NUMBER = 10;
+  public static final byte LETTER_NUMBER = 10;
 
-  @HiddenFromObjC public static final byte OTHER_NUMBER = 11;
+  public static final byte OTHER_NUMBER = 11;
 
-  @HiddenFromObjC public static final byte SPACE_SEPARATOR = 12;
+  public static final byte SPACE_SEPARATOR = 12;
 
-  @HiddenFromObjC public static final byte LINE_SEPARATOR = 13;
+  public static final byte LINE_SEPARATOR = 13;
 
-  @HiddenFromObjC public static final byte PARAGRAPH_SEPARATOR = 14;
+  public static final byte PARAGRAPH_SEPARATOR = 14;
 
-  @HiddenFromObjC public static final byte CONTROL = 15;
+  public static final byte CONTROL = 15;
 
-  @HiddenFromObjC public static final byte FORMAT = 16;
+  public static final byte FORMAT = 16;
 
-  @HiddenFromObjC public static final byte PRIVATE_USE = 18;
+  public static final byte PRIVATE_USE = 18;
 
-  @HiddenFromObjC public static final byte SURROGATE = 19;
+  public static final byte SURROGATE = 19;
 
-  @HiddenFromObjC public static final byte DASH_PUNCTUATION = 20;
+  public static final byte DASH_PUNCTUATION = 20;
 
-  @HiddenFromObjC public static final byte START_PUNCTUATION = 21;
+  public static final byte START_PUNCTUATION = 21;
 
-  @HiddenFromObjC public static final byte END_PUNCTUATION = 22;
+  public static final byte END_PUNCTUATION = 22;
 
-  @HiddenFromObjC public static final byte CONNECTOR_PUNCTUATION = 23;
+  public static final byte CONNECTOR_PUNCTUATION = 23;
 
-  @HiddenFromObjC public static final byte OTHER_PUNCTUATION = 24;
+  public static final byte OTHER_PUNCTUATION = 24;
 
-  @HiddenFromObjC public static final byte MATH_SYMBOL = 25;
+  public static final byte MATH_SYMBOL = 25;
 
-  @HiddenFromObjC public static final byte CURRENCY_SYMBOL = 26;
+  public static final byte CURRENCY_SYMBOL = 26;
 
-  @HiddenFromObjC public static final byte MODIFIER_SYMBOL = 27;
+  public static final byte MODIFIER_SYMBOL = 27;
 
-  @HiddenFromObjC public static final byte OTHER_SYMBOL = 28;
+  public static final byte OTHER_SYMBOL = 28;
 
-  @HiddenFromObjC public static final byte INITIAL_QUOTE_PUNCTUATION = 29;
+  public static final byte INITIAL_QUOTE_PUNCTUATION = 29;
 
-  @HiddenFromObjC public static final byte FINAL_QUOTE_PUNCTUATION = 30;
+  public static final byte FINAL_QUOTE_PUNCTUATION = 30;
 
   public static final byte DIRECTIONALITY_UNDEFINED = -1;
 
